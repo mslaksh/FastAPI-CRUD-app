c204bcb14cc08ec372abc1cb6cea467ea187be1c
mslaksh <90710912+mslaksh@users.noreply.github.com>
2024-03-25 12:51:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,27 @@
-from fastapi import FastAPI, HTTPException, status, Query, Response
-from pydantic import BaseModel
-from typing import Optional
-from random import randrange
+from fastapi import FastAPI
+
 app = FastAPI()
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    ratings: Optional[int] = None
-
-my_list = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "title of post 2", "content": "content of post 2", "id": 2}
-]
 @app.get("/")
-def get_all_posts():
-    return {"data": my_list}
+def root():
+    return "todooo"
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0, 1000000)
-    my_list.append(post_dict)
-    return {"data": post_dict}
+@app.post("/todo")
+def create_todo():
+    return "create todo item"
 
-@app.get("/posts/latest")
-def get_latest_post():
-    post = my_list[-1]
-    return {"post_detail": post}
+@app.get("/todo/{id}")
+def read_todo(id: int):
+    return "read todo item with id {id}"
 
-@app.get("/posts/{id}")
-def get_post_by_id(id: int):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with ID {id} not found")
-    return {"post_detail": post}
+@app.put("/todo/{id}")
+def update_todo(id: int):
+    return "update todo item with id {id}"
 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    indx = find_index_post(id)
-    if indx is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with ID {id} does not exist")
-    my_list.pop(indx)
-    return {"message": f"Post with ID {id} successfully deleted"}
+@app.delete("/todo/{id}")
+def delete_todo(id: int):
+    return "delete todo item with id {id}"
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    indx = find_index_post(id)
-    if indx is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with ID {id} does not exist")
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_list[indx] = post_dict
-    return {"message": f"Post with ID {id} successfully updated"}
-
-def find_post(id):
-    for p in my_list:
-        if p["id"] == id:
-            return p
-
-def find_index_post(id):
-    for i, p in enumerate(my_list):
-        if p['id'] == id:
-            return i
-        
+@app.get("/todo")
+def read_todo_list():
+    return "read todo list"
